0e08968b28d11ca31887bbbd0a669c2f346a067e
IlyaOsw <ilya_osw@bk.ru>
2024-01-05 11:16:38 +0200

    Updates

--- static/js/281.e4ed13c6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkshop=self.webpackChunkshop||[]).push([[281],{5259:(s,e,t)=>{t.r(e),t.d(e,{default:()=>h});t(2791);const a="ClientsPage_test__R9-NF";var c=t(184);const h=()=>(0,c.jsx)("div",{className:a})}}]);
-//# sourceMappingURL=281.e4ed13c6.chunk.js.map
--- static/js/863.29da01f7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkshop=self.webpackChunkshop||[]).push([[863],{4863:(s,e,t)=>{t.r(e),t.d(e,{default:()=>h});t(2791);const a="NewsPage_test__tmoxP";var c=t(184);const h=()=>(0,c.jsx)("div",{className:a})}}]);
-//# sourceMappingURL=863.29da01f7.chunk.js.map
